799c401eb0f14b27009580e0ab9c5395d6224f48
Aliia <cl27f27f@gmail.com>
2025-01-18 21:22:45 +0300

    Добавлена функция удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardDesc, cardImgLink, removeFunc) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = card.querySelector('.card__delete-button');
+  const cardImg = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+
+  cardImg.src = cardImgLink;
+  cardImg.alt = cardTitle;
+  cardTitle.textContent = cardDesc;
+
+  deleteButton.addEventListener('click', () => removeFunc(card));
+
+  cardsList.prepend(card);
+}
 
 // @todo: Функция удаления карточки
+function removeCard(card) {
+  console.log(card);
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+  createCard(card.name, card.link, removeCard);
+})
